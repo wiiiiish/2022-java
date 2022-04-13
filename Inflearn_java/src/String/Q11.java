dc7175e400d4476580d512bbb811f357014dd99a
김소원 <a90614@naver.com>
2022-04-14 00:24:29 +0900

    String Q11

--- Inflearn_java/src/String/Q11.java
+++ Inflearn_java/src/String/Q11.java
@@ -1,10 +1,42 @@
 package String;
 
+import java.util.Scanner;
+
 public class Q11 {
 
+	public String solution (String str) {
+		String answer = "";
+		
+		char m = str.charAt(0);
+		int n = 0;
+		
+		for (int i=0; i<str.length(); i++) {
+			if (m==str.charAt(i)) {
+				n++;
+			} else {
+				if (n>1) {
+					answer += str.charAt(i-1);
+					answer += n;
+				} else {
+					answer += str.charAt(i-1);
+				}
+				m = str.charAt(i);
+				n=1;
+			}
+			if (i == str.length()-1) {
+				answer += str.charAt(i);
+				answer += n;
+			}
+		}
+		return answer;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Q11 T = new Q11();
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		System.out.print(T.solution(str));
+		
 	}
 
 }
