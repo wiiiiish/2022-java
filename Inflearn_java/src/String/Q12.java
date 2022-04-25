8da63763dd925de78b94b1f85e6d8c02a6569952
김소원 <a90614@naver.com>
2022-04-25 23:10:52 +0900

    String Q12

---
+++ Inflearn_java/src/String/Q12.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.util.Scanner;
+
+public class Q12 {
+
+	public static void main(String[] args) {
+		Q12 T = new Q12();
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+	}
+
+}
